6a7da85b7c475371fb878042e78ea754eae2e585
yangliang913 <yangliang@zhicloud.com>
2015-02-03 14:34:07 +0800

    增加文件
    程序框架搭建

--- cloud_terminal/main.cpp
+++ cloud_terminal/main.cpp
@@ -1,10 +1,13 @@
 #include "cloud_terminal.h"
+#include "mainwindow.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	cloud_terminal w;
+	//cloud_terminal w;
+	//w.show();
+	CMainWindow w;
 	w.show();
 	return a.exec();
 }
---
+++ cloud_terminal/mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include <QLabel>
+#include <QtWidgets/QApplication>
+#include <QTextCodec>
+#include <QDesktopWidget>
+
+CMainWindow::CMainWindow(QWidget *parent)
+	: QMainWindow(parent)
+{
+	currentScreenWidth = QApplication::desktop()->width();
+	currentScreenHeight = QApplication::desktop()->height();
+	createFirstLoginWidget();
+	createSecondLoginWidget();
+	mainLayout = new QStackedLayout();
+	mainLayout->addWidget(firstLoginWidget);
+	mainLayout->addWidget(secondLoginWidget);
+	this->setLayout(mainLayout);
+	mainLayout->setCurrentIndex(0);
+	this->resize(currentScreenWidth, currentScreenHeight);
+
+	this->setWindowTitle(QStringLiteral("ÖÂÔÆ¿Æ¼¼×ÀÃæÔÆÖÕ¶Ë"));
+	this->setWindowFlags(Qt::FramelessWindowHint);
+}
+
+CMainWindow::~CMainWindow()
+{
+
+}
+
+void CMainWindow::createFirstLoginWidget()
+{
+	firstLoginWidget = new QWidget(this);
+	QLabel *label = new QLabel("firstLoginWidget", firstLoginWidget);
+}
+
+void CMainWindow::createSecondLoginWidget()
+{
+	secondLoginWidget = new QWidget(this);
+	QLabel *label = new QLabel("secondLoginWidget", secondLoginWidget);
+}
