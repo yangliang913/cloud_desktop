24f69367ab62bcbe818910043b97d0684b75b749
yangliang913 <yangliang@zhicloud.com>
2015-02-04 13:49:11 +0800

    更新代码
    修改自定义出入框代码

---
+++ cloud_terminal/mytextedit.cpp
@@ -0,0 +1,31 @@
+#include "mytextedit.h"
+
+CMyTextEdit::CMyTextEdit(QString img, QString text, int wid, int hei, QWidget *parent)
+	: QWidget(parent)
+{
+	imgPath = img;
+	width = wid;
+	height = hei;
+	showText = text;
+
+	this->setFixedSize(width, height);
+	this->setAutoFillBackground(true);
+
+	QPalette palette;
+	//palette.setColor(QPalette::Background, QColor(192, 253, 123));
+	palette.setBrush(QPalette::Background, QBrush(QPixmap(imgPath)));
+	this->setPalette(palette);
+	inputEdit = new QTextEdit();
+	//inputEdit->setAlignment(Qt::AlignHCenter);
+	inputEdit->setPlaceholderText(showText);
+	inputEdit->setStyleSheet("background-color:transparent;color:white;font-size:12px;border:0px;");
+	QHBoxLayout *hLayout = new QHBoxLayout;
+	hLayout->setAlignment(Qt::AlignCenter);
+	hLayout->addWidget(inputEdit);
+	this->setLayout(hLayout);
+}
+
+CMyTextEdit::~CMyTextEdit()
+{
+
+}
